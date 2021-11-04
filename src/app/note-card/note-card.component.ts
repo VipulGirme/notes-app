9ec7067560e628cf597e4a31ff3e190894df8c52
Vipul R Girme <49040675+VipulGirme@users.noreply.github.com>
2021-11-04 13:03:10 +0530

    Devstackr tutorial
    Video one ended

--- src/app/note-card/note-card.component.ts
+++ src/app/note-card/note-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, Input, OnInit, Renderer2, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-note-card',
@@ -7,9 +7,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NoteCardComponent implements OnInit {
 
-  constructor() { }
+
+  @Input('title') title : string;
+  @Input() body : string;
+
+  @ViewChild('truncator', { static: true }) truncator: ElementRef<HTMLElement>;
+  @ViewChild('bodyText', { static: true }) bodyText: ElementRef<HTMLElement>;
+  constructor(private renderer : Renderer2) { }
+
+
 
   ngOnInit(): void {
+    // Work out if there is a text overflow and if not, then hide truncator
+    let style = window.getComputedStyle(this.bodyText.nativeElement, null);
+    let viewableHeight = parseInt(style.getPropertyValue("height"),10);
+
+    if(this.bodyText.nativeElement.scrollHeight > viewableHeight) {
+      // if there is no text overflow
+      this.renderer.setStyle(this.truncator.nativeElement,'display', 'block');
+    } else {
+      this.renderer.setStyle(this.truncator.nativeElement,'display', 'none');
+    }
   }
 
 }

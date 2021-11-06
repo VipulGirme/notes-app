fe9088ef728429fab83ce684483d423482ef8ef2
Vipul R Girme <49040675+VipulGirme@users.noreply.github.com>
2021-11-06 16:44:48 +0530

    Working Note Card
    Note Card and Note List Details added

--- src/app/pages/note-details/note-details.component.ts
+++ src/app/pages/note-details/note-details.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
+import { NotesService } from 'src/app/services/notes.service';
+import { Note } from 'src/app/shared/note.module';
 
 @Component({
   selector: 'app-note-details',
@@ -7,9 +11,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NoteDetailsComponent implements OnInit {
 
-  constructor() { }
+  note: Note;
+  constructor(private notesService: NotesService, private router : Router) { }
 
   ngOnInit(): void {
+    this.note = new Note();
+  }
+  onSubmit(form: NgForm) {
+   this.notesService.add(form.value);
+   this.router.navigateByUrl('/');
   }
 
 }
--- src/app/pages/notes-list/notes-list.component.ts
+++ src/app/pages/notes-list/notes-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { NotesService } from 'src/app/services/notes.service';
+import { Note } from 'src/app/shared/note.module';
 
 @Component({
   selector: 'app-notes-list',
@@ -7,9 +9,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NotesListComponent implements OnInit {
 
-  constructor() { }
+  notes: Note[] = new Array<Note>();
+  constructor(private notesService : NotesService) { }
 
   ngOnInit(): void {
+    this.notes = this.notesService.getAll();
   }
 
 }

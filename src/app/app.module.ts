7465a5cbb8bdb7d1c1fb85f55559a0cbdd1fdd01
Vipul R Girme <49040675+VipulGirme@users.noreply.github.com>
2021-11-14 00:22:09 +0530

    Adding Animations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -20,7 +22,8 @@ import { NoteDetailsComponent } from './pages/note-details/note-details.componen
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/pages/notes-list/notes-list.component.ts
+++ src/app/pages/notes-list/notes-list.component.ts
@@ -1,3 +1,4 @@
+import { style, transition, trigger } from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { NotesService } from 'src/app/services/notes.service';
@@ -6,7 +7,27 @@ import { Note } from 'src/app/shared/note.module';
 @Component({
   selector: 'app-notes-list',
   templateUrl: './notes-list.component.html',
-  styleUrls: ['./notes-list.component.scss']
+  styleUrls: ['./notes-list.component.scss'],
+  animations: [
+    trigger('itemAnim', [
+      // Entry Animation
+      transition('void => *',[
+        // Initial State
+        style({
+          height: 0,
+          opacity: 0,
+          transform: 'scale(0.85)',
+          'margin-bottom': 0,
+
+          // We have to expand padding properties
+          paddingTop:0,
+          paddingBottom:0,
+          paddingRight: 0,
+          paddingLeft:0
+        })
+      ])
+    ]) 
+  ]
 })
 export class NotesListComponent implements OnInit {
 

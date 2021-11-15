711947e2088cbf80ba02d3fca42590f102caf101
Vipul R Girme <49040675+VipulGirme@users.noreply.github.com>
2021-11-16 00:16:23 +0530

    Animations WIP

--- src/app/pages/notes-list/notes-list.component.ts
+++ src/app/pages/notes-list/notes-list.component.ts
@@ -1,4 +1,4 @@
-import { style, transition, trigger } from '@angular/animations';
+import { animate, style, transition, trigger } from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { NotesService } from 'src/app/services/notes.service';
@@ -11,7 +11,7 @@ import { Note } from 'src/app/shared/note.module';
   animations: [
     trigger('itemAnim', [
       // Entry Animation
-      transition('void => *',[
+      transition('void => *', [
         // Initial State
         style({
           height: 0,
@@ -20,25 +20,34 @@ import { Note } from 'src/app/shared/note.module';
           'margin-bottom': 0,
 
           // We have to expand padding properties
-          paddingTop:0,
-          paddingBottom:0,
+          paddingTop: 0,
+          paddingBottom: 0,
           paddingRight: 0,
-          paddingLeft:0
-        })
+          paddingLeft: 0
+        }),
+        animate('50ms', style({
+          height: '*',
+          'margin-bottom': '*',
+          paddingTop: '*',
+          paddingBottom: '*',
+          paddingRight: '*',
+          paddingLeft: '*'
+        })),
+        animate(2000)
       ])
-    ]) 
+    ])
   ]
 })
 export class NotesListComponent implements OnInit {
 
   notes: Note[] = new Array<Note>();
-  constructor(private notesService : NotesService,private router: Router) { }
+  constructor(private notesService: NotesService, private router: Router) { }
 
   ngOnInit(): void {
     this.notes = this.notesService.getAll();
   }
 
-  deleteNote(id: number){
+  deleteNote(id: number) {
     this.notesService.delete(id);
   }
   //Master
